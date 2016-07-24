85d80fbcfe4d72081eb8163fd00f0829e1e0871f
Srinivasa.Kodali <Srinivasa.Kodali@IN2075209W1>
2016-07-24 17:26:36 +0530

    Add project files.

---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phoneword", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
+            
+            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.edtxt_PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.btn_TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.btn_CallButton);
+
+            callButton.Enabled = false;
+
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (String.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                // On "Call" button click, try to dial phone number.
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate {
+                    // Create intent to dial phone
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            };
+
+        }
+
+        
+    }
+}
+
---
+++ Reminder/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Reminder
+{
+    [Activity(Label = "Reminder", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+            #region Widget Inititalization
+
+            Button SaveReminder = FindViewById<Button>(Resource.Id.btn_Save);
+            TimePicker ReminderTimePicker = FindViewById<TimePicker>(Resource.Id.tp_Date);
+            TextView ReminderDescription = FindViewById<TextView>(Resource.Id.txtView_Description);
+
+            #endregion
+
+
+            #region Events
+
+            SaveReminder.Click += SaveReminder_Click;
+            
+            #endregion
+
+        }
+
+        private void SaveReminder_Click(object sender, EventArgs e)
+        {
+            var callDialog = new AlertDialog.Builder(this);
+            
+            if (Reminder.Code.ReminderOperations.Save())
+            {
+                callDialog.SetMessage("Saved Successfully");
+                callDialog.SetNeutralButton("Call", delegate {    });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            }
+            else
+            {
+                callDialog.SetMessage("Error while saving");
+                callDialog.SetNeutralButton("Call", delegate { });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            }
+
+        }
+    }
+}
+
